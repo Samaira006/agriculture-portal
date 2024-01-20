72cd32c0274166a7b3b2cc5e26188a2ba9701af8
Samaira006 <139033238+Samaira006@users.noreply.github.com>
2024-01-20 13:53:06 +0530

    Update fsend_otp.php

--- farmer/fsend_otp.php
+++ farmer/fsend_otp.php
@@ -28,9 +28,9 @@ function smtp_mailer($to,$subject, $msg){
 	$mail->Port = 465; 
 	$mail->IsHTML(true);
 	$mail->CharSet = 'UTF-8';
-	$mail->Username = "agricultureportal01@gmail.com";   
+	$mail->Username = "samairagundampadu@gmail.com";   
     $mail->Password = "password"; 	
-    $mail->SetFrom("agricultureportal01@gmail.com");  
+    $mail->SetFrom("mhvd hgjb nrlq pztj");  
 	$mail->Subject = $subject;
 	$mail->Body =$msg;
 	$mail->AddAddress($to);
